44073dbc1f91588fd1da2742b8fec63ed001f517
Kristján Oddsson <koddsson@gmail.com>
2024-03-06 21:02:09 +0100

    fuck it we ball

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,1 +1,66 @@
-// Put the code here
+import { RssReader } from "./rss-reader.js";
+
+const reader = new RssReader({
+  subscriptions: [{ url: "https://feeds.megaphone.fm/theyard" }],
+});
+
+const template = document.querySelector<HTMLTemplateElement>("#episode-row")!;
+
+for (const item of await reader.getItems()) {
+  const element = template.content.cloneNode(true) as HTMLElement;
+  element.querySelector(".title")!.textContent = item.title;
+  element.querySelector("a")?.setAttribute("data-url", item.link);
+  element.querySelector("a")?.addEventListener("click", onItemSelect);
+  document.body.append(element);
+}
+
+function onItemSelect(event: PointerEvent) {
+  const link = event.currentTarget as HTMLAnchorElement;
+  const player = document.querySelector("audio")!;
+
+  player.setAttribute("src", link.dataset.url!);
+
+  navigator.mediaSession.metadata = new MediaMetadata({
+    title: "Shadows of Ourselves",
+    artist: "The Yard",
+    artwork: [
+      {
+        src: "https://whatpwacando.today/src/img/media/mirror-conspiracy256x256.jpeg",
+        sizes: "256x256",
+        type: "image/jpeg",
+      },
+      {
+        src: "https://whatpwacando.today/src/img/media/mirror-conspiracy512x512.jpeg",
+        sizes: "512x512",
+        type: "image/jpeg",
+      },
+    ],
+  });
+
+  navigator.mediaSession.setActionHandler("play", () => player.play());
+  navigator.mediaSession.setActionHandler("pause", () => player.pause());
+  navigator.mediaSession.setActionHandler("seekbackward", (details) => {
+    const skipTime = details.seekOffset || 1;
+    player.currentTime = Math.max(player.currentTime - skipTime, 0);
+  });
+
+  navigator.mediaSession.setActionHandler("seekforward", (details) => {
+    const skipTime = details.seekOffset || 1;
+    player.currentTime = Math.min(
+      player.currentTime + skipTime,
+      player.duration,
+    );
+  });
+
+  navigator.mediaSession.setActionHandler("seekto", (details) => {
+    if (details.fastSeek && "fastSeek" in player) {
+      player.fastSeek(details.seekTime);
+      return;
+    }
+    player.currentTime = details.seekTime;
+  });
+
+  navigator.mediaSession.setActionHandler("previoustrack", () => {
+    player.currentTime = 0;
+  });
+}
---
+++ src/js/rss-reader.ts
@@ -0,0 +1,93 @@
+interface Subscription {
+  url: string;
+}
+
+interface Post {
+  title: string;
+  description: string;
+  date: Date;
+  link: string;
+}
+
+export class RssReader {
+  subscriptions: Subscription[];
+
+  constructor({ subscriptions }: { subscriptions?: Subscription[] } = {}) {
+    this.subscriptions = subscriptions || [];
+  }
+
+  async getItems() {
+    const parser = new DOMParser();
+
+    const subscriptionsXML = await this.#getSubscriptions();
+
+    const posts = [];
+
+    for (const { text } of subscriptionsXML) {
+      const document_ = parser.parseFromString(text, "application/xml");
+      for (const item of document_.querySelectorAll("item, entry")) {
+        const post = this.#getPost(item);
+        posts.push(post);
+      }
+    }
+
+    posts.sort((a, b) => b.date - a.date);
+
+    return posts;
+  }
+
+  #getPost(item: Element): Post {
+    const title = item.querySelector("title")?.textContent || "";
+    const description =
+      item.querySelector("description, content")?.textContent || "";
+    const date = new Date(
+      item.querySelector("pubDate, updated")?.textContent || "",
+    );
+    const link =
+      item.querySelector("link")?.textContent ||
+      item.querySelector("enclosure")?.getAttribute("url") ||
+      "";
+
+    return { title, description, date, link };
+  }
+
+  async #getSubscriptions() {
+    const cache = JSON.parse(localStorage.getItem("cache") || "{}");
+    const results = [];
+
+    for (const sub of this.subscriptions) {
+      if (cache[sub.url]) {
+        const then = new Date(cache[sub.url].date).getTime();
+        const now = Date.now();
+        const oneHour = 60 * 60 * 1000;
+        const moreThanOneHourAgo = now - then > oneHour;
+
+        if (moreThanOneHourAgo) {
+          try {
+            const response = await fetch(sub.url);
+            const text = await response.text();
+            cache[sub.url] = { text, date: new Date() };
+          } catch (error) {
+            console.error(`Failed to fetch`, error);
+            continue;
+          }
+        }
+      } else {
+        try {
+          const response = await fetch(sub.url);
+          const text = await response.text();
+          cache[sub.url] = { text, date: new Date() };
+        } catch (error) {
+          console.error(`Failed to fetch ${sub.url}`, error);
+          continue;
+        }
+      }
+
+      results.push(cache[sub.url]);
+    }
+
+    localStorage.setItem("cache", JSON.stringify(cache));
+
+    return results;
+  }
+}
